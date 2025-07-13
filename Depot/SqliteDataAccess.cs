36d4198cd475df11ebefe820b50c7f41b6bc1d58
roodey145 <abdu2624@outlook.com>
2025-07-13 22:11:00 +0200

    Creating SQLite db and its connection

---
+++ Depot/SqliteDataAccess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+
+namespace Depot
+{
+    public class SqliteDataAccess
+    {
+
+        public static void AddItem(string productId, string productName, string bundlePiecesText, string numberOfBundlesText, string piecesText, string priceText)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                string sql = "INSERT INTO Products (ProductId, ProductName, BundlePieces, NumberOfBundles, Pieces, Price) " +
+                             "VALUES (@ProductId, @ProductName, @BundlePieces, @NumberOfBundles, @Pieces, @Price)";
+
+                cnn.Execute(sql, new
+                {
+                    ProductId = productId,
+                    ProductName = productName,
+                    BundlePieces = bundlePiecesText,
+                    NumberOfBundles = numberOfBundlesText,
+                    Pieces = piecesText,
+                    Price = priceText
+                });
+            }
+        }
+
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
